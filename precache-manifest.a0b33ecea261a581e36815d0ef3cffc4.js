de4d5357c42dac3cb4fca4c142cb5bf14b0da195
bug Maker <451354@qq.com>
2018-11-15 23:49:49 +0800

    增加：dist

---
+++ precache-manifest.a0b33ecea261a581e36815d0ef3cffc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "986aad15e1cabe68b808",
+    "url": "/static/js/chunk-vendors.e6681abc.js"
+  },
+  {
+    "revision": "d119318adb1676b1b4dc",
+    "url": "/static/js/app.b4f656ff.js"
+  },
+  {
+    "revision": "986aad15e1cabe68b808",
+    "url": "/static/css/chunk-vendors.779f7d1d.css"
+  },
+  {
+    "revision": "d119318adb1676b1b4dc",
+    "url": "/static/css/app.f9db8af6.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4c074fb1fe2a59abd1230cb03e124a6e",
+    "url": "/index.html"
+  }
+];
