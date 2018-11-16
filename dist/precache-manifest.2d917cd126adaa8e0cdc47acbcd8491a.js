992067f0d2d399f6524d5ebe5c1e537a688d57ea
bug Maker <451354@qq.com>
2018-11-16 08:49:15 +0800

    更新：Build结果

--- dist/precache-manifest.2d917cd126adaa8e0cdc47acbcd8491a.js
+++ dist/precache-manifest.2d917cd126adaa8e0cdc47acbcd8491a.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/staff/static/js/chunk-vendors.e6681abc.js"
   },
   {
-    "revision": "3395bbe4a40cc1d69026",
-    "url": "/staff/static/js/app.bb339013.js"
+    "revision": "7bda60a69cf578f22d0a",
+    "url": "/staff/static/js/app.b0a875a9.js"
   },
   {
     "revision": "986aad15e1cabe68b808",
     "url": "/staff/static/css/chunk-vendors.779f7d1d.css"
   },
   {
-    "revision": "3395bbe4a40cc1d69026",
-    "url": "/staff/static/css/app.f9db8af6.css"
+    "revision": "7bda60a69cf578f22d0a",
+    "url": "/staff/static/css/app.17d3b047.css"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/staff/robots.txt"
   },
   {
-    "revision": "047419f83f07ece7661b37970663b226",
+    "revision": "b1ef5382abf4a8e23f52b120e6d8946b",
     "url": "/staff/index.html"
   }
 ];
