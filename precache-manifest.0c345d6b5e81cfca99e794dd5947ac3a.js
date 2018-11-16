a531facf4d58d3e5a6f3c05696ac32ace97dc70c
bug Maker <451354@qq.com>
2018-11-16 08:39:42 +0800

    更新：build结果

---
+++ precache-manifest.0c345d6b5e81cfca99e794dd5947ac3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "986aad15e1cabe68b808",
+    "url": "/staff/static/js/chunk-vendors.e6681abc.js"
+  },
+  {
+    "revision": "3395bbe4a40cc1d69026",
+    "url": "/staff/static/js/app.bb339013.js"
+  },
+  {
+    "revision": "986aad15e1cabe68b808",
+    "url": "/staff/static/css/chunk-vendors.779f7d1d.css"
+  },
+  {
+    "revision": "3395bbe4a40cc1d69026",
+    "url": "/staff/static/css/app.f9db8af6.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/staff/robots.txt"
+  },
+  {
+    "revision": "047419f83f07ece7661b37970663b226",
+    "url": "/staff/index.html"
+  }
+];
