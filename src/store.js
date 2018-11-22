0ff020bd3e260f8498824508fe1690c269b96073
bug Maker <451354@qq.com>
2018-11-22 20:31:08 +0800

    增加：音符名称出现时间值。

--- src/store.js
+++ src/store.js
@@ -1,8 +1,30 @@
+function resetSettings () {
+  const timer = 5
+  const noteCount = 1
+  if (localStorage.getItem('treble') === null) {
+    localStorage.setItem('treble', true)
+  }
+  if (localStorage.getItem('bass') === null) {
+    localStorage.setItem('bass', false)
+  }
+  if (localStorage.getItem('timer') === null) {
+    localStorage.setItem('timer', timer)
+  }
+  if (localStorage.getItem('noteNameTime') === null) {
+    localStorage.setItem('noteNameTime', timer - 1)
+  }
+  if (localStorage.getItem('noteCount') === null) {
+    localStorage.setItem('noteCount', noteCount)
+  }
+}
+resetSettings()
 export default {
   state: {
-    treble: localStorage.getItem('treble') !== 'false',
+    treble: localStorage.getItem('treble') === 'true',
     bass: localStorage.getItem('bass') === 'true',
-    timer: localStorage.getItem('timer') === null ? 5 : +localStorage.getItem('timer')
+    timer: +localStorage.getItem('timer'),
+    noteNameTime: +localStorage.getItem('noteNameTime'),
+    noteCount: +localStorage.getItem('noteCount')
   },
   setTrebleShow: function (value) {
     this.state.treble = value
@@ -15,5 +37,18 @@ export default {
   setTimer: function (value) {
     this.state.timer = value
     localStorage.setItem('timer', value)
+    // 重置音符名称出现时间
+    if (this.state.noteNameTime > value) {
+      const noteNameTime = value
+      this.setNoteNameTime(noteNameTime)
+    }
+  },
+  setNoteNameTime: function (value) {
+    this.state.noteNameTime = value
+    localStorage.setItem('noteNameTime', value)
+  },
+  setNoteCount: function (value) {
+    this.state.noteCount = value
+    localStorage.setItem('noteCount', value)
   }
 }
