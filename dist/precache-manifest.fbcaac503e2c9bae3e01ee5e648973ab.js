0847883365716aae24f9989ac78b58578b3fc14f
bug Maker <451354@qq.com>
2018-11-22 21:13:44 +0800

    更新：dist

---
+++ dist/precache-manifest.fbcaac503e2c9bae3e01ee5e648973ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c331f2da98b6796ed623",
+    "url": "/staff/static/js/chunk-vendors.468d8072.js"
+  },
+  {
+    "revision": "c05b11eff97ca0b829af",
+    "url": "/staff/static/js/app.0ff82b82.js"
+  },
+  {
+    "revision": "c331f2da98b6796ed623",
+    "url": "/staff/static/css/chunk-vendors.779f7d1d.css"
+  },
+  {
+    "revision": "c05b11eff97ca0b829af",
+    "url": "/staff/static/css/app.475c8bb3.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/staff/robots.txt"
+  },
+  {
+    "revision": "e787b6f59a066f023f3e28127fdd3ab1",
+    "url": "/staff/index.html"
+  }
+];
